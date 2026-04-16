00e4e92a82000b711e6e5bea41eb0cb21b11e902
Harish Patil <hbpatil275@gmail.com>
2026-04-16 23:55:49 +0530

    done

--- 1_Basics/6_mul_two_float_nums.c
+++ 1_Basics/6_mul_two_float_nums.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 int main(){
 
+    float num1;
+    float num2;
 
+    printf("Enter two numbers: ");
+    scanf("%f %f",&num1,&num2);
+
+    float ans=num1 * num2;
+
+    printf("The product of %f and %f is : %f",num1,num2,ans);
 
 
 
