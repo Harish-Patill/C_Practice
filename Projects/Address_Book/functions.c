991e833124ebdf0b88c733432d533d00b3ed1e36
Harish-Patill <hbpatil275@gmail.com>
2026-06-23 11:46:24 +0530

    added case handling for multiple contacts with same name in edit function, and completed search function

--- Projects/Address_Book/functions.c
+++ Projects/Address_Book/functions.c
@@ -1,32 +1,40 @@
 #include "project_header.h"
 
+void list_all(struct Contacts *eptr,int size){
+    for(int i=0;i<size;i++){
+        printf("%s %s %s\n",eptr[i].name,eptr[i].number,eptr[i].email);
+    }
+}
+
 void create_contacts(struct Contacts *eptr,int size){
     int add_contacts_loop=1;
-
+    
     while(add_contacts_loop){
-
+        
         printf("Enter Name: ");
         scanf(" %[^\n]",eptr[contact_count].name);
-    
+        
         printf("Enter Mobile Number: ");
         scanf("%s",eptr[contact_count].number);
-    
+        
         printf("Enter Email-ID: ");
         scanf("%s",eptr[contact_count].email);
         
         contact_count++;
-
+        
         printf("\nDo you want to add another contact? [1 for Yes / 0 for No]: ");
         scanf("%d",&add_contacts_loop);
     }
 }
 
 void edit_contacts(struct Contacts *eptr,int size){
-    int fount_flag=0;
+    int name_count=0;
+    int edit_flag=0;
     int user_edit_oinion=0;
-    char edit_name[20];
+    int sub_user_edit_oinion=0;
+    char edit_name[50];
     char edit_number[11];
-    char edit_email[20];
+    char edit_email[50];
     
     printf("\nSeach the Contact you want to edit using any of the below options\n1. Name\n2. Mobile Number\n3. Email-ID\nEnter Here: ");
     scanf("%d",&user_edit_oinion);
@@ -35,23 +43,88 @@ void edit_contacts(struct Contacts *eptr,int size){
     case 1:{
         printf("Enter the Name: ");
         scanf(" %[^\n]",edit_name);
-        
         for(int i=0;i<size;i++){
             if(strcmp(eptr[i].name,edit_name) == 0){
-                printf("Enter New Details Below:\n");
-                printf("Enter Name: ");
-                scanf(" %[^\n]",eptr[i].name);
-                
-                printf("Enter Mobile Number: ");
-                scanf("%s",eptr[i].number);
-                
-                printf("Enter Email-ID: ");
-                scanf("%s",eptr[i].email);
-
-                fount_flag=1;
+                name_count++;
             }
         }
-        if(!fount_flag) printf("Couldn't find any contact with name %s\n",edit_name);
+        if(name_count>1){
+            printf("\nThere are %d contacts with the same name as '%s'\n",name_count,edit_name);
+            list_all(eptr,size);
+            printf("\nEdit the contact by searching for Mobile Number or Email instead\n");
+            
+            printf("1. Mobile Number\n2. Email-ID\nSelect any of the above options: ");
+            scanf("%d",&sub_user_edit_oinion);
+            
+            switch(sub_user_edit_oinion){
+                case 1:{
+                    printf("Enter the Mobile Number: ");
+                    scanf(" %[^\n]",edit_number);
+                    
+                    for(int i=0;i<size;i++){
+                        if(strcmp(eptr[i].number,edit_number) == 0){
+                            printf("\nEnter New Details Below:\n");
+                            printf("Enter Name: ");
+                            scanf(" %[^\n]",eptr[i].name);
+                            
+                            printf("Enter Mobile Number: ");
+                            scanf("%s",eptr[i].number);
+                            
+                            printf("Enter Email-ID: ");
+                            scanf("%s",eptr[i].email);
+                            
+                            edit_flag=1;
+                        }
+                    }
+                    if(!edit_flag) printf("Couldn't find any contact with name %s\n",edit_number);
+                    break;
+                }
+                
+                case 2:{
+                    printf("Enter the Email-ID: ");
+                    scanf(" %[^\n]",edit_email);
+                    
+                    for(int i=0;i<size;i++){
+                        if(strcmp(eptr[i].email,edit_email) == 0){
+                            printf("\nEnter New Details Below:\n");
+                            printf("Enter Name: ");
+                            scanf(" %[^\n]",eptr[i].name);
+                            
+                            printf("Enter Mobile Number: ");
+                            scanf("%s",eptr[i].number);
+                            
+                            printf("Enter Email-ID: ");
+                            scanf("%s",eptr[i].email);
+                            
+                            edit_flag=1;
+                        }
+                    }
+                    if(!edit_flag) printf("Couldn't find any contact with name %s\n",edit_email);
+                    break;
+                }
+            }
+        }
+
+
+        if(name_count==1){
+            for(int i=0;i<size;i++){
+                if(strcmp(eptr[i].name,edit_name) == 0){
+                    printf("\nEnter New Details Below:\n");
+                    printf("Enter Name: ");
+                    scanf(" %[^\n]",eptr[i].name);
+                    
+                    printf("Enter Mobile Number: ");
+                    scanf("%s",eptr[i].number);
+                    
+                    printf("Enter Email-ID: ");
+                    scanf("%s",eptr[i].email);
+    
+                    edit_flag=1;
+                }
+            }
+            if(!edit_flag) printf("Couldn't find any contact with name %s\n",edit_name);
+            break;
+        }
         break;
     }
     
@@ -71,10 +144,10 @@ void edit_contacts(struct Contacts *eptr,int size){
                 printf("Enter Email-ID: ");
                 scanf("%s",eptr[i].email);
                 
-                fount_flag=1;
+                edit_flag=1;
             }
         }
-        if(!fount_flag) printf("Couldn't find any contact with name %s\n",edit_number);
+        if(!edit_flag) printf("Couldn't find any contact with name %s\n",edit_number);
         break;
     }
     
@@ -94,20 +167,77 @@ void edit_contacts(struct Contacts *eptr,int size){
                 printf("Enter Email-ID: ");
                 scanf("%s",eptr[i].email);
                 
-                fount_flag=1;
+                edit_flag=1;
             }
         }
-        if(!fount_flag) printf("Couldn't find any contact with name %s\n",edit_email);
+        if(!edit_flag) printf("Couldn't find any contact with name %s\n",edit_email);
         break;
     }
 
     default:
-    printf("Invalid Option! Please select a valid option\n");
+    printf("Invalid Option! Please enter a valid option\n");
     }
 }
 
-void list_all(struct Contacts *eptr,int size){
-    for(int i=0;i<size;i++){
-        printf("%s %s %s\n",eptr[i].name,eptr[i].number,eptr[i].email);
+
+void search_contacts(struct Contacts *eptr,int size){
+    int search_flag=0;
+    int user_search_option=0;
+    char search_name[50];
+    char search_number[50];
+    char search_email[50];
+    
+
+    printf("\nSeach the Contact using any of the below options\n1. Name\n2. Mobile Number\n3. Email-ID\nEnter Here: ");
+    scanf("%d",&user_search_option);
+
+    switch(user_search_option){
+        case 1:{
+            printf("Enter the Name: ");
+            scanf(" %[^\n]",search_name);
+            for(int i=0;i<size;i++){
+                if(strcmp(eptr[i].name,search_name) == 0){
+                    printf("%s %s %s",eptr[i].name,eptr[i].number,eptr[i].email);
+                    search_flag=1;
+                    printf("\n");
+                }
+            }
+            if(!search_flag) printf("No contact found with Name %s\n",search_name);
+            break;
+        }
+
+        case 2:{
+            printf("Enter the Number: ");
+            scanf(" %[^\n]",search_number);
+
+            for(int i=0;i<size;i++){
+                if(strcmp(eptr[i].number,search_number) == 0){
+                    printf("%s %s %s",eptr[i].name,eptr[i].number,eptr[i].email);
+                    search_flag=1;
+                    break;
+                }
+            }
+            if(!search_flag) printf("No contact found with Mobile Number %s\n",search_number);
+            break;
+        }
+
+        case 3:{
+            printf("Enter Email-ID");
+            scanf("%[^\n]",search_email);
+
+            for(int i=0;i<size;i++){
+                if(strcmp(eptr[i].email,search_email) == 0){
+                    printf("%s %s %s",eptr[i].name,eptr[i].number,eptr[i].email);
+                    search_flag=1;
+                    break;
+                }
+            }
+            if(!search_flag) printf("No contact found with Email-ID %s\n",search_email);
+            break;
+        }
+
+        default:
+            printf("Enter a Valid Input!, Try again\n");
     }
+
 }
--- Projects/Address_Book/main.c
+++ Projects/Address_Book/main.c
@@ -1,5 +1,18 @@
 #include "project_header.h"
 
+/*
+progress so far:
+
+1. Add contacts             : Done with code : Validation remaining
+2. Search contacts          : Completed;
+3. Edit contacts            : Done with code : Validation remaining
+4. Delete contacts
+5. Display contacts         : Completed;
+6. Save and exit
+7. Exit                     : Completed;
+*/
+
+
 struct Contacts contact_details[100];
 int contact_count=0;
 
@@ -9,7 +22,7 @@ int main(){
 
     printf("\n\t\t:: Address Book ::");
     while(flag){
-        printf("\n1. Add Contacts\n2. Edit Contacts\n3. Delete Contacts\n4. Display Contacts\n5. Save and Exit\n6. Exit\n\nSelect any of the above options: ");
+        printf("\n1. Add Contacts\n2. Search Contacts\n3. Edit Contacts\n4. Delete Contacts\n5. Display all Contacts\n6. Save and Exit\n7. Exit\n\nSelect any of the above options: ");
         scanf("%d",&main_option);
 
         switch(main_option){
@@ -19,15 +32,20 @@ int main(){
             }
 
             case 2:{
+                search_contacts(contact_details,contact_count);
+                break;
+            }
+
+            case 3:{
                 edit_contacts(contact_details,contact_count);
                 break;
             }
             
-            case 4:{
+            case 5:{
                 list_all(contact_details,contact_count);
                 break;
             }
-            case 6:{
+            case 7:{
                 flag=0;
                 printf("\n\t\t :: Thank You ::\n");
                 break;
@@ -39,18 +57,6 @@ int main(){
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     return 0;
