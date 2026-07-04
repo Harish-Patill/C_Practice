53d7da89aae8d13dbcca5b2070b41e73eb074dfc
Harish-Patill <hbpatil275@gmail.com>
2026-07-04 09:40:13 +0530

    array practice

--- 08_Arrays/01_read_write.c
+++ 08_Arrays/01_read_write.c
@@ -1,27 +1,28 @@
 #include<stdio.h>
-void read(int arr[], int size){
+void scan(int arr[],int size){
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
 }
-void write(int arr[], int size){
+
+void print(int arr[],int size){
     for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
+    
 }
 
 int main(){
     int size=0;
-    printf("Enter array size: ");
+    printf("Enter the array size: ");
     scanf("%d",&size);
 
     int arr[size];
-    printf("Enter array elements: ");
-    read(arr,size); 
-    // printf("\n");
-    
-    printf("The entered array elements are: ");
-    write(arr,size);    
+
+    printf("Enter the array elements: ");
+    scan(arr,size);
+    printf("The array elements are: ");
+    print(arr,size);
     printf("\n");
 
     return 0;
