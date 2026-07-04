84409042a5ada754e63922845334ef6e86cb215f
Harish-Patill <hbpatil275@gmail.com>
2026-07-04 10:12:17 +0530

    prac 5

--- 08_Arrays/05_reversed_array.c
+++ 08_Arrays/05_reversed_array.c
@@ -1,33 +1,29 @@
 #include<stdio.h>
-void swap(int arr[], int size){
-    int temporary=0;
-
+void array_swap(int arr[],int size){
     for(int i=0;i<size/2;i++){
-        temporary=arr[i];
-        arr[i]=arr[size-1-i];
-        arr[size-1-i]=temporary;
+        int temp=arr[i];
+        arr[i]=arr[size-i-1];
+        arr[size-i-1]=temp;
     }
 }
 
 int main(){
-    int size=0,temporary;
-    printf("Enter array size: ");
+    int size=0;
+    printf("Size: ");
     scanf("%d",&size);
 
     int arr[size];
-    printf("Enter the array elements: ");
+    printf("Enter array elements: ");
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
     
-    //array swap here: 
-    swap(arr,size);
-    
-    printf("The array elements in the reverse order: ");
+    array_swap(arr,size);
+    printf("Reversed array      : ");
     for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
