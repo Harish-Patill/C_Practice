a51847e5304712045d642de90cf8f798648967d1
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 18:45:20 +0530

    arrays 17

--- 08_Arrays/17_count_number_of_even_and_odd_elements.c
+++ 08_Arrays/17_count_number_of_even_and_odd_elements.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
-int my_odd(int arr[],int size){
-    int ans=0;
-    for(int i=0;i<size;i++){
-        if(arr[i]%2 != 0){
-            ans++;
-        }
-    }
-    return ans;
-}
 
-int my_even(int arr[],int size){
-    int ans=0;
+int even_count(int arr[],int size){
+    int count=0;
+
     for(int i=0;i<size;i++){
         if(arr[i]%2 == 0){
-            ans++;
+            count++;
         }
     }
-    return ans;
+
+    return count;
+}
+
+int odd_count(int arr[],int size){
+    int count=0;
+
+    for(int i=0;i<size;i++){
+        if(arr[i]%2 != 0){
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int main(){
     int size=0;
-    printf("Enter size: ");
+    printf("Size: ");
     scanf("%d",&size);
 
     int arr[size];
-    printf("Enter array elements: ");
+    printf("Enter the array elements: ");
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
 
-    int odd=my_odd(arr, size);
-    int even=my_even(arr,size);
-    printf("The total number of odd elements from the array: %d\nThe total number of even elements from the array: %d\n",odd,even);
+    printf("Number of Even elements in the array: %d\nNumber of Odd elements in the array: %d\n",even_count(arr,size),odd_count(arr,size));
 
-	return 0;
+    return 0;
 }
