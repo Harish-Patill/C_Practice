9994768ff88cfb08e78771ebaab87ca4fa0cf7fa
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 18:38:34 +0530

    arrays 16

--- 08_Arrays/16_unique_elements_from_two_arrays.c
+++ 08_Arrays/16_unique_elements_from_two_arrays.c
@@ -1,41 +1,53 @@
 #include<stdio.h>
-void unique(int arr1[],int arr2[],int s1,int s2){
+int main(){
+    int size1=0;
+    int size2=0;
     
-    for(int i=0;i<s1;i++){
+    printf("Size for 1st array: ");
+    scanf("%d",&size1);
+    
+    int arr1[size1];
+    printf("Enter array elements: ");
+    for(int i=0;i<size1;i++){
+        scanf("%d",&arr1[i]);
+    }
+    
+    printf("Size for 2nd array: ");
+    scanf("%d",&size2);
+    
+    int arr2[size2];
+    printf("Enter array elements: ");
+    for(int i=0;i<size2;i++){
+        scanf("%d",&arr2[i]);
+    }
+    
+    printf("Unique elements from both the arrays: ");
+    for(int i=0;i<size1;i++){                                                      // this prints unique elements from the array 1
         int flag=0;
-        for(int j=0;j<s2;j++){
+        for(int j=0;j<size2;j++){
             if(arr1[i] == arr2[j]){
                 flag=1;
                 break;
             }
         }
-        if(flag == 0) printf("%d ",arr1[i]);
-    }
-}
-
-int main(){
-    int size1=0;
-    int size2=0;
-
-    printf("Enter array 1 size: ");
-    scanf("%d",&size1);
-
-    int arr1[size1];
-    printf("Enter array 1 elements: ");
-    for(int i=0;i<size1;i++){
-        scanf("%d",&arr1[i]);
+        if(flag == 0){
+            printf("%d ",arr1[i]);
+        }
     }
 
-    printf("Enter array 2 size: ");
-    scanf("%d",&size2);
-
-    int arr2[size2];
-    printf("Enter array 2 elements: ");
-    for(int i=0;i<size2;i++){
-        scanf("%d",&arr2[i]);
+    for(int i=0;i<size2;i++){                                                     // this prints unique elements from the array 2
+        int flag=0;
+        for(int j=0;j<size1;j++){
+            if(arr2[i] == arr1[j]){
+                flag=1;
+                break;
+            }
+        }
+        if(flag == 0){
+            printf("%d ",arr2[i]);
+        }
     }
-
-    unique(arr1,arr2,size1,size2);
+    printf("\n");
 
     return 0;
 }
