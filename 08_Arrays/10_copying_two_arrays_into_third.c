4f7f3e8f4847bd4885a7110662d7c51d24bf5e32
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 17:24:03 +0530

    array 10

--- 08_Arrays/10_copying_two_arrays_into_third.c
+++ 08_Arrays/10_copying_two_arrays_into_third.c
@@ -2,41 +2,38 @@
 int main(){
 	int size1=0;
 	int size2=0;
-	
-	printf("Enter array_1 size: ");
+
+	printf("Array 1 size: ");
 	scanf("%d",&size1);
-	
+
 	int arr1[size1];
-	printf("Enter array_1 elements: ");
+	printf("Enter array elemnts: ");
 	for(int i=0;i<size1;i++){
 		scanf("%d",&arr1[i]);
 	}
 	
-	printf("Enter array_2 size: ");
+	printf("Array 2 size: ");
 	scanf("%d",&size2);
 	
 	int arr2[size2];
-	printf("Enter array_2 elements: ");
+	printf("Enter array elemnts: ");
 	for(int i=0;i<size2;i++){
 		scanf("%d",&arr2[i]);
 	}
-	
-	int count=0;
-	int new_size=size1+size2;
-	int new_array[new_size];
-	for(int i=0;i<size1;i++){
-		new_array[i]=arr1[i];
-		count++;
+
+	int arr[size1+size2];
+	for(int i=0;i<size1;i++){					//first array elements
+		arr[i]=arr1[i];
 	}
-	for(int i=0;i<size2;i++){
-		new_array[i+count]=arr2[i];
+	for(int i=0;i<size2;i++){				//second array elements
+		arr[i+size1]=arr2[i];
 	}
-	
-	printf("New Array elements are: ");
-	for(int i=0;i<new_size;i++){
-		printf("%d ",new_array[i]);
+
+	printf("The final array elements are: ");
+	for(int i=0;i<size1+size2;i++){
+		printf("%d ",arr[i]);
 	}
-	
+	printf("\n");
 
 	return 0;
 }
