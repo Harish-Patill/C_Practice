3941e33d078dd0e5ef8178756f645e84c6cfac3a
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 17:11:37 +0530

    array 09

--- 08_Arrays/09_copy_to_another_array.c
+++ 08_Arrays/09_copy_to_another_array.c
@@ -1,35 +1,29 @@
 #include<stdio.h>
+void array_copying(int arr[],int new_array[], int size){
+    for(int i=0;i<size;i++){
+        new_array[i]=arr[i];
+    }
+}
+
 int main(){
     int size=0;
-    int size1=0;
-
-    printf("Enter the size of array: ");
+    printf("Size: ");
     scanf("%d",&size);
-    
+
     int arr[size];
-    int arr_1[size];
+    int new_array[size];
+
     printf("Enter the array elements: ");
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
     
-    printf("Copying the array elements into another array: \n");
-    
-    for(int i=0;i<size;i++){
-        arr_1[i]=arr[i];
-    }
-    
-    printf("Original array elements: \n");
+    array_copying(arr,new_array,size);
+    printf("The new array elements  : ");
     for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
     
-    printf("Copied array elements: \n");
-    for(int i=0;i<size;i++){
-        printf("%d ",arr_1[i]);
-    }
-    printf("\n");
-
     return 0;
 }
