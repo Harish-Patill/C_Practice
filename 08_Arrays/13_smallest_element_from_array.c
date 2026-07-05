f29a0beb4cfcd2840f17af1fee6afddc0fe1f42d
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 17:42:36 +0530

    array 13

--- 08_Arrays/13_smallest_element_from_array.c
+++ 08_Arrays/13_smallest_element_from_array.c
@@ -1,28 +1,27 @@
 #include<stdio.h>
-int my_smallest(int arr[],int n){
-	int smallest=arr[0];
-	for(int i=0;i<n;i++){
-		if(arr[i]<smallest){
-			smallest=arr[i];
-		}
-	}
-	return smallest;
+int smallest_element(int arr[],int size){
+    int min=arr[0];
+
+    for(int i=0;i<size;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    return min;
 }
 
 int main(){
-	
-	int size=0;
-	printf("Enter Size: ");
-	scanf("%d",&size);
+    int size=0;
+    printf("Size: ");
+    scanf("%d",&size);
 
-	int arr[size];
-	printf("Enter array elements: ");
-	for(int i=0;i<size;i++){
-		scanf("%d",&arr[i]);
-	}
+    int arr[size];
+    printf("Enter the array elements: ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
 
-	int smallest=my_smallest(arr,size);
-	printf("The Smallest element from the array is %d\n",smallest);
+    printf("The Smallest element from the array: %d\n",smallest_element(arr,size));
 
-	return 0;
+    return 0;
 }
