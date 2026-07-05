bec3e736bb938c5b6ec52f5a1681c160212bbf74
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 18:50:49 +0530

    array  18

--- 08_Arrays/18_unique_elemts_from_array.c
+++ 08_Arrays/18_unique_elemts_from_array.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
-
-void unique_elemts(int arr[],int size){
+int main(){
+    int size=0;
+    
+    printf("Size: ");
+    scanf("%d",&size);
+    
+    int arr[size];
+    printf("Enter array elements: ");
     for(int i=0;i<size;i++){
-
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Non Repeated elements from the array: ");
+    for(int i=0;i<size;i++){
         int count=0;
         for(int j=0;j<size;j++){
             if(arr[i] == arr[j]){
@@ -14,21 +24,6 @@ void unique_elemts(int arr[],int size){
             printf("%d ",arr[i]);
         }
     }
-}
-
-int main(){
-    int size=0;
-    printf("Enter the array size: ");
-    scanf("%d",&size);
-
-    int arr[size];
-    printf("Enter array elements: ");
-    for(int i=0;i<size;i++){
-        scanf("%d",&arr[i]);
-    }
-
-    printf("Unique elements from the Array: ");
-    unique_elemts(arr,size);
     printf("\n");
 
     return 0;
