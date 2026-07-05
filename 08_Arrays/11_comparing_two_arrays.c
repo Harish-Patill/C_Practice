dda6c0df5e927ac89468ca93dfc7a70614eef74d
Harish-Patill <hbpatil275@gmail.com>
2026-07-05 17:34:34 +0530

    arrays 11

--- 08_Arrays/11_comparing_two_arrays.c
+++ 08_Arrays/11_comparing_two_arrays.c
@@ -1,41 +1,47 @@
 #include<stdio.h>
+
+int comparison(int arr1[],int size1,int arr2[]){
+	int flag=0;
+
+	for(int i=0;i<size1;i++){
+		if(arr1[i] != arr2[i]){
+			flag=1;
+		}
+	}
+	return flag;
+}
+
 int main(){
 	int size1=0;
 	int size2=0;
-	int flag=0;
 
-	printf("Enter array_1 size: ");
+	printf("Enter array 1 size: ");
 	scanf("%d",&size1);
-	
 	int arr1[size1];
-	printf("Enter array_1 elements: ");
+	
+	printf("Enter array elemnts: ");
 	for(int i=0;i<size1;i++){
 		scanf("%d",&arr1[i]);
 	}
 	
-	printf("Enter array_2 size: ");
+	printf("Enter array 2 size: ");
 	scanf("%d",&size2);
-	
 	int arr2[size2];
-	printf("Enter array_2 elements: ");
+	
+	printf("Enter array elemnts: ");
 	for(int i=0;i<size2;i++){
 		scanf("%d",&arr2[i]);
 	}
-    
-    if(size1 != size2){
-        printf("NOT equal\n");
-        return 0;
-    }
 
-    for(int i=0;i<size2;i++){
-        if(arr1[i] != arr2[i]){
-            flag=1;
-            break;
-        }
-    }
-	
-    if(flag) printf("NOT EQUAL\n");
-    else printf("EQUAL\n");
+	if(size1 != size2){
+		printf("Both the array's are Not Equal\n");
+		return 0;
+	}
+	if(comparison(arr1,size2,arr2)){
+		printf("Both the array's are Not Equal ff\n");
+		return 0;
+	}
+	printf("Both the array's are Equal\n");
 
 	return 0;
 }
