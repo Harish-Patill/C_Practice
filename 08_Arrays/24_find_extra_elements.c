509d63ca4c8565a4be35bc1d5ed51808a242037d
Harish-Patill <hbpatil275@gmail.com>
2026-07-10 01:32:49 +0530

    arrays: 24 25

---
+++ 08_Arrays/24_find_extra_elements.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+void array_extra_elements(int arr_1[],int size_1,int arr_2[],int size_2){
+
+    for(int i=0;i<size_1;i++){
+        int flag=1;
+        for(int j=0;j<size_2;j++){
+            if(arr_1[i] == arr_2[j]){
+                flag=0;
+            }
+        }
+        if(flag){
+            printf("%d ",arr_1[i]);
+        }
+    }
+}
+
+int main(){
+    int size_1=0;
+    int size_2=0;
+
+    printf("Size 1: ");
+    scanf("%d",&size_1);
+    
+    int arr_1[size_1];
+    printf("Enter array elements: ");
+    for(int i=0;i<size_1;i++){
+        scanf("%d",&arr_1[i]);
+    }
+
+    printf("Size 2: ");
+    scanf("%d",&size_2);
+
+    int arr_2[size_2];
+    printf("Enter array elements: ");
+    for(int i=0;i<size_2;i++){
+        scanf("%d",&arr_2[i]);
+    }
+
+    array_extra_elements(arr_1,size_1,arr_2,size_2);
+    printf("\n");
+
+    return 0;
+}
+
+
+// Size 1: 5
+// Enter array elements: 1 2 3 4 5
+// Size 2: 4
+// Enter array elements: 1 2 3 4
+// 5 
---
+++ 08_Arrays/25_equilibrium_index.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int equilibrium_index(int arr[], int size){
+    int total_sum = 0;
+    for(int i = 0; i < size; i++){
+        total_sum=total_sum + arr[i];
+    }
+
+    int left_sum = 0;
+    for(int i = 0; i < size; i++){
+        int right_sum=total_sum - left_sum - arr[i];
+
+        if(left_sum == right_sum){
+            return i;
+        }
+
+        left_sum=left_sum + arr[i];
+    }
+
+    return -1;  // no equilibrium index found
+}
+
+int main(){
+    int size = 0;
+    printf("Size: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter array elements: ");
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The Equilibrium index of array is %d\n", equilibrium_index(arr, size));
+
+    return 0;
+}
