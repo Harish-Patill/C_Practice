d0b437b9874953f048620ae01f54b53b08dc4fbf
Harish-Patill <hbpatil275@gmail.com>
2026-07-10 12:49:07 +0530

    qsort

---
+++ 09_Functions/01_qsort_function.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare(const void *a,const void *b){
+    int x=*(int *)a;
+    int y=*(int *)b;
+
+    return x-y;     // to sort in ascending order 
+    return y-x;     // to sort in descending order
+}
+
+int main(){
+    int size=0;
+    printf("Enter the array Size: ");
+    scanf("%d",&size);
+
+    int arr[size];
+    printf("Enter the array elements: ");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    
+    qsort(arr,size,sizeof(arr[0]),compare);
+    
+    
+    printf("Sorted the array elements: ");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
