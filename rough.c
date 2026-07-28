987678a0d58cc7fa0271c96c81bbbd7278cc4a40
Harish-Patill <hbpatil275@gmail.com>
2026-07-29 00:42:14 +0530

    array 28

--- rough.c
+++ rough.c
@@ -1,9 +1,60 @@
-#include<stdio.h>
+// Read two sorted arrays and print only the elements that are present in both arrays. Each common element should be printed only once.
+
+// Input:
+// arr1 = 1 2 2 3 5 7
+// arr2 = 2 2 4 5 6 7
+
+// Output:
+// 2 5 7
 
 
-#define b 58
-int main()
-{
-    #define a 16
-    printf("%d\n",a);
+
+
+#include <stdio.h>
+void unique_elements(int arr1[],int size1,int arr2[],int size2){
+    for(int i=0;i<size1;i++){
+        int printed=0;
+        for(int j=0;j<i;j++){
+            if(arr1[i] == arr1[j]){
+                printed=1;
+                break;
+            }
+        }
+
+        if(printed){
+            continue;
+        }
+
+        for(int j=0;j<size2;j++){
+            if(arr1[i] == arr2[j]){
+                printf("%d ",arr1[i]);
+                break;
+            }
+        }
+    }
+}
+
+int main(){
+    int size1=0,size2=0;
+
+    printf("Enter array 1 size: ");
+    scanf("%d",&size1);
+
+    int arr1[size1];
+    for(int i=0;i<size1;i++){
+        scanf("%d",&arr1[i]);
+    }
+
+    printf("Enter array 2 size: ");
+    scanf("%d",&size2);
+
+    int arr2[size2];
+    for(int i=0;i<size2;i++){
+        scanf("%d",&arr2[i]);
+    }
+
+    unique_elements(arr1,size1,arr2,size2);
+    printf("\n");
+
+    return 0;
 }
