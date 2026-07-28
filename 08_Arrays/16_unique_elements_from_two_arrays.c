821908dd4e4fdf443fd1ec6a566f9c56e594e01f
Harish-Patill <hbpatil275@gmail.com>
2026-07-28 11:13:47 +0530

    prac till 16

--- 08_Arrays/16_unique_elements_from_two_arrays.c
+++ 08_Arrays/16_unique_elements_from_two_arrays.c
@@ -22,31 +22,59 @@ int main(){
     }
     
     printf("Unique elements from both the arrays: ");
-    for(int i=0;i<size1;i++){                                                      // this prints unique elements from the array 1
-        int flag=0;
-        for(int j=0;j<size2;j++){
-            if(arr1[i] == arr2[j]){
-                flag=1;
+    for(int i=0;i<size1;i++){                               // for array 1
+        int printed = 0;
+
+                                                                        
+        for(int k=0;k<i;k++){                               // Check if already printed
+            if(arr1[i] == arr1[k]){
+                printed=1;
                 break;
             }
         }
-        if(flag == 0){
-            printf("%d ",arr1[i]);
+
+        if(printed)
+            continue;
+
+        int found = 0;
+
+        for(int j=0;j<size2;j++){                            // Check if present in arr2
+            if(arr1[i] == arr2[j]){
+                found=1;
+                break;
+            }
         }
+
+        if(!found)
+            printf("%d ",arr1[i]);
     }
 
-    for(int i=0;i<size2;i++){                                                     // this prints unique elements from the array 2
-        int flag=0;
-        for(int j=0;j<size1;j++){
-            if(arr2[i] == arr1[j]){
-                flag=1;
+    for(int i=0;i<size2;i++){                               // for array 2
+        int printed = 0;
+
+        for(int k=0;k<i;k++){                               // Check if already printed
+            if(arr2[i] == arr2[k]){
+                printed=1;
                 break;
             }
         }
-        if(flag == 0){
-            printf("%d ",arr2[i]);
+
+        if(printed)
+            continue;
+
+        int found = 0;
+
+        for(int j=0;j<size1;j++){                           // Check if present in arr1
+            if(arr2[i] == arr1[j]){
+                found=1;
+                break;
+            }
         }
+
+        if(!found)
+            printf("%d ",arr2[i]);
     }
+
     printf("\n");
 
     return 0;
---
+++ rough.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+
+#define b 58
+int main()
+{
+    #define a 16
+    printf("%d\n",a);
+}
