d43508de0e3530bce76d22dce2ecd179e71428fb
Harish-Patill <hbpatil275@gmail.com>
2026-07-28 22:12:21 +0530

    prac till 20

--- 08_Arrays/20_remove_duplicate_elements.c
+++ 08_Arrays/20_remove_duplicate_elements.c
@@ -1,45 +1,42 @@
 #include<stdio.h>
-int remove_duplicate(int arr[],int size,int arr1[]){
+int remove_duplicates(int arr[],int arr1[],int size){
     int new_index=0;
-    for(int i=0;i<size;i++){        
-        int stored=0;
 
-        for(int k=0;k<i;k++){
-            if(arr[k] == arr[i]){
-                stored=1;
+    for(int  i=0;i<size;i++){
+        int added=0;
+
+        for(int j=0;j<i;j++){
+            if(arr[i] == arr[j]){
+                added=1;
                 break;
             }
         }
 
-        if(stored == 0){
+        if(added == 0){
             arr1[new_index]=arr[i];
-            stored=1;
             new_index++;
         }
     }
 
     return new_index;
-
 }
 
 int main(){
     int size=0;
-    int temp=0;
-    
     printf("Size: ");
     scanf("%d",&size);
-    
+
     int arr[size];
     printf("Enter array elements: ");
     for(int i=0;i<size;i++){
         scanf("%d",&arr[i]);
     }
-    
-    int size1=size;
-    int arr1[size1];
-    int new_index=remove_duplicate(arr,size,arr1);
-    printf("Array elements after removing the duplicate elements: ");
-    for(int i=0;i<new_index;i++){
+
+    int arr1[size];
+    int new_size=remove_duplicates(arr,arr1,size);
+
+    printf("Array after removing duplicates: ");
+    for(int i=0;i<new_size;i++){
         printf("%d ",arr1[i]);
     }
     printf("\n");
